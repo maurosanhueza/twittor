84f5e27e679703f3a7964a25d0d8e3e448a71806
Mauro Sanhueza <maurosanhueza@gmail.com>
2019-08-07 11:15:32 -0400

    fix

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts( 'js/sw-utils.js');
 
 
 const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -44,24 +44,27 @@ self.addEventListener('install', e => {
 //activamos el sw
 
 self.addEventListener('activate', e => {
-
-    console.log('activate');
+ 
     const respuesta = caches.keys().then( keys => {
-
+ 
         keys.forEach( key => {
-
-            console.log(key);
+ 
             if (  key !== STATIC_CACHE && key.includes('static') ) {
-
                 return caches.delete(key);
             }
-
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
         });
-
+ 
     });
-
+ 
     e.waitUntil( respuesta );
+ 
 });
+
 
 //verficamos los fetch con network fallback
 
