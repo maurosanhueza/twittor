de666b49f57b2dcbc965e0ade49ccafaeaa7ce90
Mauro Sanhueza <maurosanhueza@gmail.com>
2019-08-07 10:34:51 -0400

    app base

---
+++ sw.js
@@ -0,0 +1,85 @@
+importScripts( 'js/sw-utils.js');
+
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js'
+
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+//generamos la instalacion
+
+self.addEventListener('install', e => {
+
+    console.log('install');
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then( cache => cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then( cache => cache.addAll( APP_SHELL_INMUTABLE ));
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+
+});
+
+//activamos el sw
+
+self.addEventListener('activate', e => {
+
+    console.log('activate');
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            console.log(key);
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+});
+
+//verficamos los fetch con network fallback
+
+self.addEventListener( 'fetch' , e => {
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if( res ) {
+            return res;
+        }else{
+            return fetch( e.request.url ).then( newRes => {
+                
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes);
+
+            });
+        } 
+        
+    });
+
+    e.respondWith( respuesta );
+});
